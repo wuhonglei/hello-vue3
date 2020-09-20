ce85faaca3fe10acf7052ed46742fccc504633ba
wuhonglei <1017368065@qq.com>
2020-09-20 22:41:25 +0800

    空项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<h1>{{ msg }}</h1>
+		<button @click="count++">count is: {{ count }}</button>
+		<p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+	</div>
+</template>
+
+<script>
+
+let person = {};
+console.info(person?.name ?? 'wuhonglei');
+
+export default {
+	  name: "HelloWorld",
+	props: {
+		msg: String
+	},
+	data() {
+		return {
+			count: 0
+		}
+	}
+}
+</script>
