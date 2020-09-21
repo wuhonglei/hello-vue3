35af49a84cbc91935c9890ae38efc68e82a21a0e
wuhonglei <1017368065@qq.com>
2020-09-22 00:06:16 +0800

    场景header头部区域

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+	<i-header></i-header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import iHeader from './views/header.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		iHeader
+	}
 }
 </script>
---
+++ src/views/header.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="header-wrapper">
+		<!-- 顶部导航 -->
+		<nav class="top-navigation my-5 mx-4">
+            <div class="flex justify-between">
+                <a href="/">EchoCloud回声云</a>
+                <i class="fas fa-bars"></i> 
+            </div>
+        </nav>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="postcss" scoped>
+.header-wrapper {
+    height: 500px;
+    background-image: linear-gradient(to bottom, var(--purple), var(--indigo));
+}
+
+.top-navigation {
+	height: 81px;
+	line-height: 41px;
+}
+
+@media screen and (min-width: 1024;) {
+    .top-navigation {
+        padding-left: 69px;
+        padding-right: 69px;
+    }
+}
+</style>
