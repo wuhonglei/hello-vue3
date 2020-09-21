ef4c682a78e291bc45e5c21dfc5422da295438ad
wuhonglei <1017368065@qq.com>
2020-09-22 00:24:28 +0800

    完成了头部背景

--- src/views/header.vue
+++ src/views/header.vue
@@ -1,12 +1,17 @@
 <template>
 	<div class="header-wrapper">
 		<!-- 顶部导航 -->
-		<nav class="top-navigation my-5 mx-4">
-            <div class="flex justify-between">
-                <a href="/">EchoCloud回声云</a>
-                <i class="fas fa-bars"></i> 
-            </div>
-        </nav>
+		<nav class="top-navigation py-5 px-4">
+			<div class="nav-container h-full flex justify-between items-center">
+				<a
+					href="/"
+					class="brand-text text-white text-xl font-bold"
+				>EchoCloud回声云</a>
+				<button class="py-1 px-3">
+					<i class="fas fa-bars text-white"></i>
+				</button>
+			</div>
+		</nav>
 	</div>
 </template>
 
@@ -18,19 +23,30 @@ export default {
 
 <style lang="postcss" scoped>
 .header-wrapper {
-    height: 500px;
-    background-image: linear-gradient(to bottom, var(--purple), var(--indigo));
+	height: 500px;
+	background-image: linear-gradient(to bottom, #697ded, #5e30c1);
 }
 
 .top-navigation {
 	height: 81px;
-	line-height: 41px;
 }
 
 @media screen and (min-width: 1024;) {
-    .top-navigation {
-        padding-left: 69px;
-        padding-right: 69px;
-    }
+	.top-navigation {
+		padding-left: 69px;
+		padding-right: 69px;
+	}
+}
+
+.nav-container {
+	margin: 0 34px;
+}
+
+.brand-text {
+	letter-spacing: 3px;
+}
+
+.fa-bars {
+    font-size: 13px;
 }
 </style>
